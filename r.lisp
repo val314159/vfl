ee3ece88c85d7b9ad0409ce4d53b9f0b835da2ce
Joel Ward <jmward@gmail.com>
2019-11-07 21:40:13 -0800

    update

--- r.lisp
+++ r.lisp
@@ -2,12 +2,31 @@
 	    (:shadow #:read-symbol))
 (in-package :r)
 
+(defun read-no-tab-ws (_ &optional _2)
+  (cl:case (peekc _)
+    (#\Tab             (error "NO TABS"))
+    ((#\Space
+      #\Return)        (:L* (readc _) (read-no-tab-ws _ _2)))
+    (#\Newline (if? _2 (:L* (readc _) (read-no-tab-ws _ _2))))
+    (t                 (peekc _))))
+
+(defun is-ws (_) (find _ '(#\Space #\Newline #\Tab #\Return eof)))
+
 (defun is-sym (_)
-  (not (ife (find _ '(#\Space #\Newline #\Tab eof)) t
-	    (get-macro-character _)
-	    (:1 (mv-list (get-macro-character _))))))
+  (mv-bind (macro non-term) (get-macro-character _)
+	   (ife macro     non-term
+		(is-ws _) nil
+		t         t)))
 
 (defun read-symbol (_ &optional _2)
-  (ife (null   _2)         (read-symbol (readc _))
-       (is-sym _2) (cons _ (read-symbol (readc _)))
+  (ife (null   _2)        (read-symbol (readc _))
+       (is-sym _2) (:L* _ (read-symbol (readc _)))
        t           (unread-char _2 _)))
+
+(defun read-col (_)
+  (loop :for x = (read-ex-ws _)
+	:until {x === eof}
+	:do (fmt "[[~A]]~%" x)
+	:collect x
+	:until {(read-no-tab-ws _) === #\Newline}))
+
