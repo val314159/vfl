86609a44a693cad487df570b9a82352c89adef77
Joel Ward <jmward@gmail.com>
2019-11-11 22:12:47 -0800

    update

--- w2.lisp
+++ w2.lisp
@@ -1,8 +1,12 @@
-(ql:quickload '(:woo :clack :vfl) :silent cl:t)
+
+
+
+(ql:quickload (cl:quote (:woo :clack)) :silent cl:t)
+(ql:quickload (cl:quote (:vfl)))
 (cl:in-package :vu)
 (install-macro-chars)
 (defun run-program (&rest rest)
   (uiop:wait-process (uiop:launch-program (apply #λconcat-string rest))))
-(load-file "wi99")
+(load-file "wi")
 (launch)
 (loop (sleep 60))
